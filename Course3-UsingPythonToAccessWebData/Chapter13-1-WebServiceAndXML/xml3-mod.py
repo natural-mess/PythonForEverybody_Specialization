f3d3af50c5df50f5ac76e5a572aaa71bf4b69ee8
Phuc Le <37369211+phucle1303@users.noreply.github.com>
2024-11-17 10:29:00 +0100

    finished chapter 13

---
+++ Course3-UsingPythonToAccessWebData/Chapter13-1-WebServiceAndXML/xml3-mod.py
@@ -0,0 +1,25 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+#nums = list()
+count = 0
+sum = 0
+for result in counts:
+    # Debug print the data :)
+    #print(result.text)
+    count = count + 1
+    sum = sum + int(result.text)
+
+print('Count:', count)
+print('Sum:', sum)
